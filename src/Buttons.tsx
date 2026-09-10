dddc0196f66db21830520d9e524af678be2bb813
Kamvelihle <gwijgwij011@gmail.com>
2026-09-10 10:52:00 +0200

    test git user

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -8,7 +8,7 @@ export interface ButtonProps {
     onClick: React.MouseEventHandler<HTMLButtonElement>;
     title?: string;
     text?: string;
-    icon?: React.ReactNode; // i recommend users use lucide-react for this
+    icon?: React.ReactNode;
     isFill?: boolean;
     active?: boolean;
 };
